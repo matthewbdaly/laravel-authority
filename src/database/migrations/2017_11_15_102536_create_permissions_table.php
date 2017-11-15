a988cac0cc395fd7734c4f52f6b5d5e04b2999aa
Matthew Daly <matthewbdaly@gmail.com>
2017-11-15 10:29:44 +0000

    Added permission model and table

--- src/database/migrations/2017_11_15_102536_create_permissions_table.php
+++ src/database/migrations/2017_11_15_102536_create_permissions_table.php
@@ -15,6 +15,8 @@ class CreatePermissionsTable extends Migration
     {
         Schema::create('permissions', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('label');
             $table->timestamps();
         });
     }

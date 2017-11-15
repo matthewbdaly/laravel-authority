3fe0ca13ff405e7c4c0b54345fb15104af16ec75
Matthew Daly <matthewbdaly@gmail.com>
2017-11-15 11:17:30 +0000

    Added failing test for attaching permissions to role

--- tests/Unit/Eloquent/Models/RoleTest.php
+++ tests/Unit/Eloquent/Models/RoleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\Eloquent\Models;
 
 use Tests\TestCase;
+use Matthewbdaly\LaravelAuthority\Eloquent\Models\Permission;
 use Matthewbdaly\LaravelAuthority\Eloquent\Models\Role;
 
 class RoleTest extends TestCase
@@ -16,5 +17,21 @@ class RoleTest extends TestCase
         $saved = Role::first();
         $this->assertEquals('admin', $saved->name);
         $this->assertEquals('Admin', $saved->label);
+    }
+
+    public function testRolePermissions()
+    {
+        $role = factory(Role::class)->create([
+            'name' => 'admin',
+            'label' => 'Admin'
+        ]);
+        $permission = factory(Permission::class)->create([
+            'name' => 'admin',
+            'label' => 'Admin'
+        ]);
+        $role->permissions()->save($permission);
+        $saved = Role::first();
+        $this->assertEquals('admin', $saved->permissions->first()->name);
+        $this->assertEquals('Admin', $saved->permissions->first()->label);
     }
 }

92ef98685ba631b7c399ba25f062c69bb55eadc0
Matthew Daly <matthewbdaly@gmail.com>
2017-11-15 09:53:21 +0000

    Added user fixture and factory

---
+++ tests/factories/ModelFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Tests\Fixtures\User::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(Matthewbdaly\LaravelChat\Eloquent\Models\Message::class, function (Faker $faker) {
+    return [
+    ];
+});

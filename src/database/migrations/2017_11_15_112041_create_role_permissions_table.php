d78267269ff2600bae62ea879d7217c31ee734be
Matthew Daly <matthewbdaly@gmail.com>
2017-11-15 11:23:43 +0000

    Added foreign keys to role permissions table

--- src/database/migrations/2017_11_15_112041_create_role_permissions_table.php
+++ src/database/migrations/2017_11_15_112041_create_role_permissions_table.php
@@ -16,6 +16,8 @@ class CreateRolePermissionsTable extends Migration
         Schema::create('role_permissions', function (Blueprint $table) {
             $table->integer('role_id')->unsigned();
             $table->integer('permission_id')->unsigned();
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+            $table->foreign('permission_id')->references('id')->on('permissions')->onDelete('cascade');
             $table->timestamps();
         });
     }
